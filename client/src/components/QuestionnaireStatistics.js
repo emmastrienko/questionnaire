38b23334448baa7410bbc01ec48456d913536291
Emma Strienko <125968910+emmastrienko@users.noreply.github.com>
2025-03-20 17:59:28 +0200

    Update QuestionnaireStatistics.js

--- client/src/components/QuestionnaireStatistics.js
+++ client/src/components/QuestionnaireStatistics.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { getQuestionnaireStatistics } from "../services/api";
 import { Bar, Pie } from "react-chartjs-2";
 import {
